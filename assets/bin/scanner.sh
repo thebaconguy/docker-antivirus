4250805f59f66ecd70afc32487d03eef4966adec
thebaconguy <18342079+thebaconguy@users.noreply.github.com>
2018-01-27 00:47:46 -0500

    Copy files instead of moving them
    This allows files to not be moved unless they are absolutely harmful (e.g. doesn't disrupt seeding).
    Based on this comment: https://www.linkedin.com/feed/update/urn:li:article:8952422602466404009?commentUrn=urn%3Ali%3Acomment%3A%28article%3A8952422602466404009%2C6287350188021415936%29&replyUrn=urn%3Ali%3Acomment%3A%28article%3A8952422602466404009%2C6311483225298604032%29

--- assets/bin/scanner.sh
+++ assets/bin/scanner.sh
@@ -5,20 +5,26 @@ then
     printf "Found files to process\n"
     for file in "/data/av/queue"/* ; do
         filename=`basename $file`
-        mv -f $file "/data/av/scan/${filename}"
+        # Copy the file instead of moving it
+        # mv -f $file "/data/av/scan/${filename}"
+        cp -f $file "/data/av/scan/${filename}"
         printf "Processing /data/av/scan/${filename}\n"
         /usr/local/bin/scanfile.sh > /data/av/scan/info 2>&1
         if [ -e "/data/av/scan/${filename}" ]
         then
             printf "  --> File ok\n"
-            mv -f "/data/av/scan/${filename}" "/data/av/ok/${filename}"
-            printf "  --> File moved to /data/av/ok/${filename}\n"
-            rm /data/scan/info
+            # Leave the original and delete the copy
+            # mv -f "/data/av/scan/${filename}" "/data/av/ok/${filename}"
+            rm -f "/data/av/scan/${filename}"
+            printf "  --> Copied file deleted /data/av/ok/${filename}\n"
+            rm -f /data/av/scan/info
         elif [ -e "/data/av/quarantine/${filename}" ]
         then
             printf "  --> File quarantined / nok\n"
-            mv -f "/data/av/scan/info" "/data/av/nok/${filename}"
-            printf "  --> Scan report moved to /data/av/nok/${filename}\n"
+            # Delete the original and save the scan log
+            rm -f $file
+            mv -f "/data/av/scan/info" "/data/av/nok/${filename}.info"
+            printf "  --> Scan report moved to /data/av/nok/${filename}.info\n"
         fi
     done
     printf "Done with processing\n"
